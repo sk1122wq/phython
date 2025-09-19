3c2e38f608318598faa6b3906de53b80e6e7b585
Muhammad Ibrahim <samiibrahim1882@gmail.com>
2025-09-19 10:20:40 +0500

    Add files via upload

---
+++ command.py
@@ -0,0 +1,5 @@
+print("Hello world")#print function this is single command 
+"""" this is multiple  command"""
+# print("Hello world") short key ctrl L + forward silag
+# print("Hello world")
+# print("Hello world")
