e1a08d4520fd56a5fa67401ffbbc9d1de3293ffe
Muhammad Ibrahim <samiibrahim1882@gmail.com>
2025-09-19 10:21:24 +0500

    Add files via upload

---
+++ condition.py
@@ -0,0 +1,25 @@
+# if condition
+#age=19
+#if(age >= 18):
+ #   print("can vote & apply for license")
+# elif condition
+#light="gree
+"""marks=int( input("enter student marks:"))
+if(marks>=90):
+    grade="A"
+elif(marks>=80 and marks<90):
+    grade="B"
+elif(marks>=70 and marks<80):
+    grade="C"
+else:
+    grade="D"
+print("grade of the student->",grade)"""
+# nesitng if(c)
+#             if(c)
+#                 print
+age=34
+if(age>=18):
+    if(age>=80):
+        print("cannot be derive")
+    else:
+        print("can dreive")
