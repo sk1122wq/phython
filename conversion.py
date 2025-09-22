9a840bf51d66b4711251c6e16e554db74225e0c6
Muhammad Ibrahim <samiibrahim1882@gmail.com>
2025-09-22 12:11:30 +0500

    Add files via upload

---
+++ conversion.py
@@ -0,0 +1,11 @@
+# type conversion (convert one type of variable to another  and this automatically conversion)
+a=2
+b=4.25
+sum=a+b # 2.0+4.25 ->> 6.25 and float is superior 
+print(sum)
+# we can add float value to string
+#Type casting(to convert float value into string or int so  we use type casting)
+c=int("2")
+d=4.25
+print(type(a))
+print(a+b)
