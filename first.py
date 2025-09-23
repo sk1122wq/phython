aa66f85671492c64415ed48f0dc1a2057e872cd2
Muhammad Ibrahim <samiibrahim1882@gmail.com>
2025-09-23 13:00:05 +0500

    Add files via upload

---
+++ first.py
@@ -0,0 +1,13 @@
+print("Hello world")
+name="ali"
+age=23
+old=True
+a=None
+print(type(old))
+print(type(a))
+price=25.99
+print("my name is",name)
+print("my age is",age)
+print(type(name))
+print(type(age))
+print(type(price))
