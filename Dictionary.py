bdc1c43aa9e7545b438f8e177f51c5752c212731
Muhammad Ibrahim <samiibrahim1882@gmail.com>
2025-10-02 14:04:42 +0500

    Add files via upload

---
+++ Dictionary.py
@@ -0,0 +1,22 @@
+# dictionary are used to store data values in key.value pairs . They are uotdered , mutable(changeable) and don't allow duplicate keys. 
+info = {
+    "key":"value",
+    "name": "apnacollege",
+    "learning":"coding"
+}
+print(info)
+# nested dictionary 
+student= {
+    "name": "rahul kumar",
+    "subjects":{
+        "phy":97,
+        "chem":98,
+        "math":95,
+    }
+}
+print(student)
+# Dictionary method 
+# myDict.keys()
+# myDict.items()
+# myDict.get("key")
+# myDict.update(newDict)
