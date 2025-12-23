706aa84495b9bc06c7b6ee8fa7669bf939731de5
Muhammad Ibrahim <samiibrahim1882@gmail.com>
2025-12-23 11:30:57 +0500

    Add files via upload

---
+++ Break and continue.py
@@ -0,0 +1,25 @@
+#  Break: used to terminate the loop when encountered 
+# i=1
+# while i<=5:
+#     print(i)
+#     if(i==3):
+#         break
+#     i+=1
+
+
+# print("end of loop")
+#  continue: terminate execution in the current iteration and continue execution of the loop with the next iteration 
+# i=0
+# while i<=5:
+#     if(i==3):
+#         i+=1
+#         continue
+#     print(i)
+#     i+=1
+# i=0
+# while i<=10:
+#      if(i%2==0):
+#         i+=1
+#         continue
+#      print(i)
+#      i+=1 
