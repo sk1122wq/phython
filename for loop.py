30d5b55e3bf29b3101d3093d003f585b802067de
Muhammad Ibrahim <samiibrahim1882@gmail.com>
2025-12-24 09:14:12 +0500

    Add files via upload

---
+++ for loop.py
@@ -0,0 +1,47 @@
+#  for loop (are used for sequential traversal for travering list, list string tuple etc)
+# list=[1,2,3,4,5]
+
+# for val in list:
+#     print(val)
+# list=[1,2,3,4,5]
+
+# for val in list:
+#     print(val)
+# else:
+#     print("End")
+#  else need in break statement
+# str="apnacollege"
+# for char in str:
+#     if(char =='o'):
+#         print("o found")
+#         break
+#     print(char)
+# else:
+#     print(End)
+#  range() range function returns a sequence of numbers, string from 0 by default and increment by 1(by default), and stop before a specified number 
+# seq=range(5)
+# print(seq[0])
+# print(seq[1])
+# print(seq[2])
+# print(seq[3])
+# print(seq[4])
+# range(start?,stop,step?)
+# for i in range(10):
+#     print(i)
+# for i in range(2,10):
+#     print(i)
+#  print the number from 1 to 100 by using for loop
+# for i in range(1,11):
+#     print(i)
+# print the number the from 1 to 100
+# for i in range(1,101,+1):
+#     print(i)
+#  print the number from 100 to 1
+# for i in range(100,0,-1):
+#      print(i)
+#  print the multiplication table of a number n
+n=int(input("enter number:"))
+for i in range(1,11):
+    print(n*i)
+
+    
