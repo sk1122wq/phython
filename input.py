3bfec8e6e9e23d4d8f84b3e797721b203ea1f380
Muhammad Ibrahim <samiibrahim1882@gmail.com>
2025-12-25 10:03:45 +0500

    Add files via upload

---
+++ input.py
@@ -0,0 +1,36 @@
+# input in python
+#name=input("enter your name:")
+#print("your name is:",name)
+# input() statement is used to except values (using keyboard) from user
+#input() result for input is always a str
+# int(input()) int
+# float (input()) float
+# parctices question
+# write a program to input 2 numbers and print their sum
+"""a=3
+b=5
+sum=a+b
+print("sum of two number is:",sum)
+
+"""
+"""first=int(input("enter first number:"))
+second=int(input("enter second number:"))
+sum=first+second
+print("sum of two number is ",sum)"""
+# wap to input of a square and print its area
+"""side=float(input("enter square side:,"))
+print("area=",side*side)"""
+# wirte a program to input 2 floating point number and print their average
+"""a=float(input("enter first number:",))
+
+b=float(input("enter second number:",))
+avg=a+b/2
+print("The average of two number is:",avg)"""
+a=int(input("enter PRICE OF LAptope:"))
+#b=int(input("enter second:"))
+#print(a>=b)
+
+s=a+4
+print(s)
+
+
