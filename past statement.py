3a5c7d830570c7940d20e34d8e0811fa7f2ac40d
Muhammad Ibrahim <samiibrahim1882@gmail.com>
2025-12-27 09:11:36 +0500

    Add files via upload

---
+++ past statement.py
@@ -0,0 +1,20 @@
+#  pass is a null statemnt that does nothing it is used as a placeholder for future code.
+# for i in range(5):
+#     pass
+# print("some useful work")
+# WAP to find of first n natural numbers.(using for)
+# n=5
+# sum=0
+# for i in range(1,n+1):
+#     sum += i
+
+# print("total sum=",sum)
+#  WAp to find factorial of first n numbers.(using for)
+n=5
+fact=1
+i=1
+while i<=n:
+    fact*=i
+    i+=1
+
+print("factorial =",fact)
