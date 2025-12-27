1f3fc5cddb6631158a002fc54a42996d5a6aa27c
Muhammad Ibrahim <samiibrahim1882@gmail.com>
2025-12-27 09:10:18 +0500

    Add files via upload

---
+++ operator.py
@@ -0,0 +1,22 @@
+# type: ignore #Arthmatic operator(+,-,*,/,%,**)
+a=5
+b=2
+sum=a+b
+print(sum)
+print(a/b) #when any number are in python the answer will be in fration
+print(a % b) #remander
+print(a**b)#power operator a^b
+# Relation operator(==,!=,>,<,>=,<=) relation operator always give output true or false(boolean value)
+print(a==b)
+print(a!=b)
+#aaaignment operators(=,+=,-=,*=,%=,**=)
+num=10
+num+=10
+print("num is:",num)
+#logical operator(not,and,or) it always work on boolean operator
+print(not False)
+print(not True)
+val1 = True
+val2 = True
+print("AND operator:",val1 and val2)
+print("OR operator:",val1 or val2)
