7a865e9886dd6970504544da17279644c9099ebb
Muhammad Ibrahim <samiibrahim1882@gmail.com>
2025-12-28 09:11:54 +0500

    Add files via upload

---
+++ practic2.py
@@ -0,0 +1,24 @@
+
+# wAP to cehck if a number entered by the user is odd or even 
+
+'''num=int(input("enter the number:"))
+if(num%2==0):
+    print("The number is even")
+else:
+    print("The number is odd")'''
+    # WAP to find the gratest of 3 number entered by the user 
+'''a=int(input("enter the first number"))
+b=int(input("enter the second  number"))
+c=int(input("enter the third  number"))
+if(a>b and a>c):
+    print("first number is gratest",a)
+elif(b>a and b>c):
+    print("b is the gratest number",b)
+else:
+    print("c is gratest number",c)'''
+# WAp to check if a number is a multiple of 7 or not
+num=int(input("enter a number:"))
+if(num%7==0):
+    print("The number is multiple of 7")
+else:
+    print("The number is not multiple of 7")
