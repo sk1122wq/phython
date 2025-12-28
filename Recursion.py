8f2e3010686dfa2c325cd11119b16c81317fd6e6
Muhammad Ibrahim <samiibrahim1882@gmail.com>
2025-12-28 09:13:09 +0500

    Add files via upload

---
+++ Recursion.py
@@ -0,0 +1,20 @@
+# def show(n):
+#     if(n ==0):
+#         return
+#     print(n)
+#     show(n-1)
+
+#     show(5)
+# def fact(n):
+#     if(n==0 or n==1):
+#         return 1
+#     else:
+#         return n * fact(n-1)
+#     print(fact(4))
+# Wrtie a recursion function to calculate the sum of first n natural number
+# def calc_sum(n):
+#     if(n==0):
+#         return 0
+#     calc_sum(n-1)+n
+#     sum=calc_sum(5)
+#     print(sum)  
