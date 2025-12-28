855dd2f9db0c74b4cb692e7f71803289911c0db5
Muhammad Ibrahim <samiibrahim1882@gmail.com>
2025-12-28 09:10:27 +0500

    Add files via upload

---
+++ practic1.py
@@ -0,0 +1,6 @@
+# write to input users firstname & print its length
+firstname=input("enter your first name")
+print("your first name is:",firstname)
+# wite to find the occurrence of '$' in a string
+str="Hi, $ i am the $ symbol $99.99"
+print(str.count("$"))
