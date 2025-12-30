9b3b2432e0a25b1e08ecba6d6d6b7ad5fd7e3ba5
Muhammad Ibrahim <samiibrahim1882@gmail.com>
2025-12-30 09:06:53 +0500

    Add files via upload

---
+++ set.py
@@ -0,0 +1,11 @@
+# set in python ( set is the collection of the unorded items)
+# Each element in the set must be unique and immutable we can not add list and dictionary
+collection={ 1,2,3,4}
+print(collection)
+print(type(collection))
+# ignore duplicate value
+# set method
+# set.add(el)
+# set.remove() 
+# set.clear()
+# set.pop()
