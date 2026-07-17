321ce5eb1f4a665096ad88329d35457864484b01
Muhammad Ibrahim <samiibrahim1882@gmail.com>
2026-07-17 18:12:16 +0500

    Add files via upload

---
+++ function.py
@@ -0,0 +1,49 @@
+# def my_function():
+#     print("Hello world")
+# my_function()
+# my_function()
+# my_function()
+# def my_function(fahrenheit):
+#     return(fahrenheit-32)*5/9
+# print(my_function(77))
+# def my_function(fname):
+#     print(fname+"Refnes")
+# my_function("Email")
+# def my_function(fname,lname):
+#     print(fname+lname)
+# my_function("Email")
+# def fun(x,y):
+#     return x+y
+# result=fun(5,3)
+# print(result)
+# def fun():
+#     return ["apple","banna","cherry"]
+# fruit=fun()
+# print(fruit[0])
+# x=range(10) # range(start,stop,step)
+# # print(x)
+# print(list(x)) # convert the display to list
+# x=range(3,10)
+# # print(x)
+# print(list(x))
+# x=range(3,10,2)
+# print(list(x))
+# using ranges(Ranges are often used in for loop ro iterate over a sequence of numbers)
+# for i in range(10):
+#     print(i)
+# python Date(A python date is not a data type of its own but we can import a module named datetime to work dates as date object )
+# import datetime
+# x=datetime.datetime.now()
+# print(x)
+# python Date(A python date is not a data type of its own but we can import a module named datetime to work dates as date object )
+# import datetime
+# x=datetime.datetime.now()
+# print(x.year)
+# print(x.strftime("%A"))
+# creating object 
+import datetime
+x=datetime.datetime(2020,5,17)
+print(x)
+
+
+
