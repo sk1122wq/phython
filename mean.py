d36dbcd1084e8f8639be0d7f932be7684c5fdb6c
Muhammad Ibrahim <samiibrahim1882@gmail.com>
2026-07-24 16:46:16 +0500

    Add files via upload

---
+++ mean.py
@@ -0,0 +1,8 @@
+# Mean(mean is the average value )
+# for group data x=upperlimit+lowerlimit/2
+# for ungroup x=sum of all number/number of sum
+# 2-6 x=2+6/2
+import numpy
+speed=[2-6,5-9]
+x=numpy.mean(speed)
+print(x)
